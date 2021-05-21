5f771ed6c2c623d51df7f99c9363048b24890e49
Diego Aviles <davil022@fiu.edu>
2021-05-21 00:10:30 -0400

    added more contestants

--- fantasy/src/App.js
+++ fantasy/src/App.js
@@ -1,4 +1,9 @@
-import jujiball from './jujiball.jpg';
+import jujiball from './profile_pics/jujiball.jpg';
+import toji from './profile_pics/toji.png';
+import gojo from './profile_pics/gojo.jpg';
+import nobara from './profile_pics/nobara.jpg';
+import megumi from './profile_pics/megumi.png';
+import yuji from './profile_pics/yuji.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +12,17 @@ function App() {
       <header className="App-header">
         <h1>Welcome to the Culling Game</h1>
         <img src={jujiball} alt="Juju"/>
-        
+        <h3>Contestant 1: </h3>
+        <img src={toji} alt="Fushiguro"/>
+        <h3>Contestant 2: </h3>
+        <img src={gojo} alt="Satoru"/>
+        <h3>Contestant 3: </h3>
+        <img src={nobara} alt="kugisaki"/>
+        <h3>Contestant 4: </h3>
+        <img src={megumi} alt="son"/>
+        <h3>Contestant 5: </h3>
+        <img src={yuji} alt="itadori"/>
+
       </header> 
     </div>
   );
